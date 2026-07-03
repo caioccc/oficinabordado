c5948937961358f6a67081391438d3c3c5b6fc7e
Caio Marinho <caiomarinho8@gmail.com>
2026-07-03 15:04:14 -0300

    firs commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// next.config.js
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+module.exports = {
+  images: {
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
